93737dfefbf5c23819c16cabd52298830d54f608
yunus arslan <yunus.arslan061@hotmail.com>
2019-12-20 23:59:14 +0300

    Add project files.

---
+++ DesignPatterns.Builder/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDirectory directory = new ProductDirectory();
+            var builder = new CustomerProductBuilder();
+
+            directory.GenerateProduct(builder);
+
+            var model = builder.GetModel();
+
+            Console.WriteLine(model.ID);
+            Console.WriteLine(model.CategoryName);
+            Console.WriteLine(model.ProductName);
+            Console.WriteLine(model.UnitPrice);
+            Console.WriteLine(model.DiscountedPrice);
+
+            Console.ReadLine();
+        }
+    }
+    class ProductModel
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal DiscountedPrice { get; set; }
+        public string CategoryName { get; set; }
+    }
+
+    abstract class ProductBuilder
+    {
+        public abstract void GetProductData();
+        public abstract void ApplyDiscount();
+        public abstract ProductModel GetModel();
+    }
+
+    class CustomerProductBuilder : ProductBuilder
+    {
+        ProductModel model = new ProductModel();
+
+        public override void ApplyDiscount()
+        {
+            model.DiscountedPrice = model.UnitPrice * (decimal)0.80;
+        }
+
+        public override ProductModel GetModel()
+        {
+            return model;
+        }
+
+        public override void GetProductData()
+        {
+            model.ID = 1;
+            model.CategoryName = "Burger";
+            model.ProductName = "Tavuk Burger";
+            model.UnitPrice = 18;
+        }
+    }
+    class ProductDirectory
+    {
+        public void GenerateProduct(ProductBuilder productBuilder)
+        {
+            productBuilder.GetProductData();
+            productBuilder.ApplyDiscount();
+        }
+    }
+}
---
+++ DesignPatterns.SinglePatterns/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SinglePatterns
+{
+  /*
+    Singleton Desenini Ne Zaman Tercih Etmeliyim
+
+     1.Sadece bir sınıfın örneği olduğu durumlarda
+     2.Bu örneğe kontrollü erişimin sağlanması durumunda
+     3. Daha sonraki geliştirme aşamalarında birden fazşa örenğe ihtiyacımız odluğunda
+     4.Kontrol , başka bir mekanizma değil, somutlaştırılmış sınıfta yerleştirilemlidir
+        */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var systemManager = SystemManager.CreateSingletonObject();
+            systemManager.Save();
+
+        }
+    }
+    class SystemManager
+    {
+        private static SystemManager _systemManager; //field
+
+        public SystemManager() //Amaç dış erişime kapatmak
+        {
+            
+        }
+
+        //Aşağıda singleton örneğini oluşturacak static nesne yaratıyoruz
+        public static SystemManager CreateSingletonObject()
+        {
+            //Bu methodun içerisinde şunu kontrol ettik. Eğer system mager nesnesi daha önceden varsa, çağrıldığında da bdaha önceden oluşturulmuş nesneyi vereceğim, yoksa da oluşturup öyle göndereceğiz.
+            if (_systemManager == null)
+            {
+                _systemManager = new SystemManager();
+            }
+
+            return _systemManager;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved...!");
+            Console.ReadKey();
+        }
+    }
+
+}
